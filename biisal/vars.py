9be2e397baf12ed695aa8f6242d5e3597e0e51e8
Sanchit0102 <137860157+Sanchit0102@users.noreply.github.com>
2024-01-11 12:46:08 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -13,18 +13,18 @@ silent_auto_grp = "https://t.me/+CZH0JaSwih44ZTM1" #Auto Filter Group Link
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', ''))
-    API_HASH = str(getenv('API_HASH', ''))
-    BOT_TOKEN = str(getenv('BOT_TOKEN' , ''))
-    name = str(getenv('name', ''))
+    API_ID = int(getenv('API_ID', '25833520'))
+    API_HASH = str(getenv('API_HASH', '7d012a6cbfabc2d0436d7a09d8362af7'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN' , '6636524134:AAEGYg4i3ZBzIkqGWAV4PSDqadHswCjhsWQ'))
+    name = str(getenv('name', 'DSFile-To-Link'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '10000'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', ''))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001925180658'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1001925180658'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1562935405").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'THE_DS_OFFICIAL'))
@@ -40,8 +40,8 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', ''))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'bisal_files')) 
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://trumbot:trumbot@cluster0.cfkaeno.mongodb.net/?retryWrites=true&w=majority'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'THE_SILENT_TEAMS')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
     BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))   
     
